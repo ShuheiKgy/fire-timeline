e71849b540d0d16dadf54ff70ba23cdfff77b70e
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-11-01 22:48:28 +0900

    initPost実装

--- public/main.js
+++ public/main.js
@@ -41,6 +41,68 @@ async function initAuth() {
   }
 }
 
+async function initPost() {
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+
+  const $postBox = document.getElementById('post-box');
+  const $post = document.getElementById('post');
+  const $postButton = document.getElementById('post-button');
+
+  const user = auth.currentUser;
+  const userRef = db.collection('users').doc(user.uid);
+
+  async function updateUserTL() {
+      const id = getProfilePageId();
+      if ((id || user.uid) !== user.uid) {
+          $postBox.style.display = 'none';
+          try {
+              const user = await db.collection('users').doc(id).get();
+              const data = user.data();
+              if (!data) {
+                  location.href = '/404.html';
+              }
+              const profile = Object.assign({ uid: id }, data);
+              updateProfile(profile);
+          } catch (err) {
+              console.warn(err);
+              location.href = '/404.html';
+              return
+          }
+      } else {
+          $postBox.style.display = '';
+          updateProfile(user);
+      }
+  }
+
+  await updateUserTL();
+
+  window.addEventListener('hashchange', async () => {
+      await updateUserTL();
+  });
+
+  $postButton.addEventListener('click', async () => {
+      // 二重投稿の予防
+      $post.disabled = true;
+      $postButton.disabled = true;
+      const text = $post.value;
+      const postRef = userRef.collection('timeline').doc();
+      try {
+          await postRef.set({
+              uid: user.uid,
+              text,
+              created: firebase.firestore.FieldValue.serverTimestamp()
+          });
+      } catch (err) {
+          showError(err);
+      } finally {
+          $post.value = ''; // clear
+          $post.disabled = false;
+          $postButton.disabled = false;
+      }
+  });
+};
+
 async function main() {
   await initAuth();
 }
