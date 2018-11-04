9db86f1f29cd125c1307c125221ea707e75fee41
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-11-04 22:59:11 +0900

    postTimeline function実装

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,62 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config());
+
+const db = new admin.firestore.Firestore();
+db.settings({ timestampsInSnapshots: true });
+
+exports.postTimeline = functions.firestore
+  .document('users/{userId}/timeline/{postId}').onCreate((snap, context) => {
+    const userId = context.params.userId;
+    const postId = context.params.postId;
+    const postData = snap.data();
+    const batchSize = 500;
+
+    if (userId !== postData.uid) {
+      return Promise.resolve();
+    }
+
+    const followersRef = db.collection('users').doc(userId).collection('followers');
+
+    function postToFollowersTimeline(prevRef, resolve, reject) {
+      let ref = followersRef.orderBy('__name__').limit(batchSize);
+      if (prevRef) {
+        ref = ref.startAfter(prevRef);
+      }
+      ref.get()
+        .then(snap => {
+          if (snap.size === 0) {
+            return null;
+          }
+
+          const batch = db.batch();
+          let prevRef = null;
+          snap.forEach(doc => {
+            prevRef = doc;
+            console.log('set:post', 'postId', postId, 'userId', userId);
+            const postRef = db.collection('users').doc(doc.id).collection('timeline').doc(postId);
+            batch.set(postRef, postData);
+          });
+
+          return batch.commit().then(() => {
+            return prevRef;
+          });
+        })
+        .then(prevRef => {
+          if (!prevRef) {
+            resolve();
+            return;
+          }
+
+          process.nextTick(() => {
+            postToFollowersTimeline(prevRef, resolve, reject);
+          });
+        })
+        .catch(reject);
+    }
+
+    return new Promise((resolve, reject) => {
+      postToFollowersTimeline(null, resolve, reject);
+    });
+  });
